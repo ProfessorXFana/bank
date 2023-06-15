e5e9ba980660db93452dd5467f2a7966c06a5525
Professor <sajal.voice@gmail.com>
2023-06-15 18:19:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const loginButton = document.getElementById('login');
+loginButton.addEventListener("click", function(){
+    const submitArea = document.getElementById('login-area');
+    submitArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+})
+
+// diposit
+
+const addDeposit = document.getElementById('addDiposit');
+addDeposit.addEventListener('click', function(){
+    const dipositNumber = getInputNumber('DepositAmount');
+  
+    updateSpanText('currentDiposit', dipositNumber);
+    updateSpanText('currentBalance', dipositNumber);
+
+    document.getElementById('DepositAmount').value = "";
+
+})
+
+function getInputNumber(id){
+    const Amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(Amount);
+    return amountNumber
+}
+
+function updateSpanText(id, dipositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const total = dipositNumber + currentNumber;
+    document.getElementById(id).innerText = total;
+    
+}
+
+// Withdraw
+
+const withdrawBtn = document.getElementById('addWithdraw');
+withdrawBtn.addEventListener('click', function(){
+    const withdrawnumber = getInputNumber('withdrawAmount');
+
+    updateSpanText('currentWithdraw', withdrawnumber);
+    updateSpanText('currentBalance', -1 * withdrawnumber);
+
+    document.getElementById('withdrawAmount').value = "";
+
+})
+
